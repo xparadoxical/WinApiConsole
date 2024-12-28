d422bb82de34245aeec776605d800f83808bfca1
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2024-12-28 01:20:53 +0100

    Separate handle-unrelated stuff from the handle type

---
+++ WinApiConsole/ConsoleInputReader.cs
@@ -0,0 +1,64 @@
+﻿using System.Buffers;
+using System.ComponentModel;
+
+using TerraFX.Interop.Windows;
+
+using WinapiConsole;
+
+namespace WinApiConsole;
+public class ConsoleInputReader
+{
+	public Task WhenInputAvailable()
+	{
+		var tcs = new TaskCompletionSource();
+		var registration = ThreadPool.RegisterWaitForSingleObject(_eventHandle, (_, _) => tcs.SetResult(), null, -1, true);
+
+		var t = tcs.Task;
+		t.ContinueWith(_ => registration.Unregister(null));
+		return tcs.Task;
+	}
+
+	public unsafe IEnumerator<INPUT_RECORD> ReadEachInput(Action<INPUT_RECORD> recordHandler)
+	{
+		var inputHandle = GetStdInput();
+		var recordsAvailable = new PinnableBox<uint>(0);
+		unsafe
+		{
+			fixed (uint* pRecordsAvailable = recordsAvailable)
+				if (!Windows.GetNumberOfConsoleInputEvents(inputHandle, pRecordsAvailable))
+					throw new Win32Exception();
+		}
+
+		scoped Span<INPUT_RECORD> buffer;
+		INPUT_RECORD[]? bufferArray = null;
+		if (recordsAvailable <= 64)
+			buffer = stackalloc INPUT_RECORD[(int)recordsAvailable.Value];
+		else
+		{
+			bufferArray = ArrayPool<INPUT_RECORD>.Shared.Rent((int)recordsAvailable.Value);
+			buffer = bufferArray.AsSpan(..(int)recordsAvailable.Value);
+		}
+
+		var recordsRead = new PinnableBox<uint>(0);
+		unsafe
+		{
+			fixed (INPUT_RECORD* pBuffer = buffer)
+			fixed (uint* pRecordsRead = recordsRead)
+			{
+				const int CONSOLE_READ_NOWAIT = 0x0002;
+				if (!Interop.Console.ReadConsoleInputEx(inputHandle, pBuffer, recordsAvailable, pRecordsRead, CONSOLE_READ_NOWAIT))
+				{
+					if (bufferArray is not null)
+						ArrayPool<INPUT_RECORD>.Shared.Return(bufferArray);
+					throw new Win32Exception();
+				}
+			}
+		}
+
+		foreach (var record in buffer)
+			yield return record;
+
+		if (bufferArray is not null)
+			ArrayPool<INPUT_RECORD>.Shared.Return(bufferArray);
+	}
+}
--- WinApiConsole/SafeConsoleInputHandle.cs
+++ WinApiConsole/SafeConsoleInputHandle.cs
@@ -1,20 +1,15 @@
-﻿using System.Buffers;
-using System.ComponentModel;
-
-using Microsoft.Win32.SafeHandles;
+﻿using Microsoft.Win32.SafeHandles;
 
 using TerraFX.Interop.Windows;
 
 namespace WinapiConsole;
 public class SafeConsoleInputHandle : SafeHandleZeroOrMinusOneIsInvalid
 {
-	private EventWaitHandle _eventHandle;
-
 	private SafeConsoleInputHandle(bool ownsHandle) : base(ownsHandle) { }
 
 	public SafeConsoleInputHandle() : this(false)
 	{
-		SetHandle(GetStdInput());
+		RefetchHandle();
 		_eventHandle = new EventWaitHandle(false, EventResetMode.ManualReset) //TODO don't waste a handle each time
 		{
 			SafeWaitHandle = new SafeWaitHandle(handle, false)
@@ -24,63 +19,14 @@ public class SafeConsoleInputHandle : SafeHandleZeroOrMinusOneIsInvalid
 	protected override bool ReleaseHandle()
 		=> throw new NotSupportedException();
 
-	private HANDLE GetStdInput()
+	public nint Handle => RefetchHandle();
+
+	private HANDLE RefetchHandle()
 	{
 		const uint STD_INPUT_HANDLE = unchecked((uint)-10);
-		return Windows.GetStdHandle(STD_INPUT_HANDLE);
-	}
 
-	public Task WhenInputAvailable()
-	{
-		var tcs = new TaskCompletionSource();
-		var registration = ThreadPool.RegisterWaitForSingleObject(_eventHandle, (_, _) => tcs.SetResult(), null, -1, true);
-
-		var t = tcs.Task;
-		t.ContinueWith(_ => registration.Unregister(null));
-		return tcs.Task;
-	}
-
-	public unsafe IEnumerator<INPUT_RECORD> ReadEachInput(Action<INPUT_RECORD> recordHandler)
-	{
-		var inputHandle = GetStdInput();
-		var recordsAvailable = new PinnableBox<uint>(0);
-		unsafe
-		{
-			fixed (uint* pRecordsAvailable = recordsAvailable)
-				if (!Windows.GetNumberOfConsoleInputEvents(inputHandle, pRecordsAvailable))
-					throw new Win32Exception();
-		}
-
-		scoped Span<INPUT_RECORD> buffer;
-		INPUT_RECORD[]? bufferArray = null;
-		if (recordsAvailable <= 64)
-			buffer = stackalloc INPUT_RECORD[(int)recordsAvailable.Value];
-		else
-		{
-			bufferArray = ArrayPool<INPUT_RECORD>.Shared.Rent((int)recordsAvailable.Value);
-			buffer = bufferArray.AsSpan(..(int)recordsAvailable.Value);
-		}
-
-		var recordsRead = new PinnableBox<uint>(0);
-		unsafe
-		{
-			fixed (INPUT_RECORD* pBuffer = buffer)
-			fixed (uint* pRecordsRead = recordsRead)
-			{
-				const int CONSOLE_READ_NOWAIT = 0x0002;
-				if (!Interop.Console.ReadConsoleInputEx(inputHandle, pBuffer, recordsAvailable, pRecordsRead, CONSOLE_READ_NOWAIT))
-				{
-					if (bufferArray is not null)
-						ArrayPool<INPUT_RECORD>.Shared.Return(bufferArray);
-					throw new Win32Exception();
-				}
-			}
-		}
-
-		foreach (var record in buffer)
-			yield return record;
-
-		if (bufferArray is not null)
-			ArrayPool<INPUT_RECORD>.Shared.Return(bufferArray);
+		var handle = Windows.GetStdHandle(STD_INPUT_HANDLE);
+		SetHandle(handle);
+		return handle;
 	}
 }
